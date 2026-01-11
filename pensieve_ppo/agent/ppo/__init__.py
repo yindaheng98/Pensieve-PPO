4075e7f996c00e86f9d6c3289433d9fdfe75be6f
yindaheng98 <yindaheng98@gmail.com>
2026-01-11 12:19:13 -0800

    use variable

---
+++ pensieve_ppo/agent/ppo/__init__.py
@@ -0,0 +1,16 @@
+"""PPO (Proximal Policy Optimization) algorithm implementation.
+
+This module provides the PPO agent and its neural network models.
+
+Reference:
+    https://github.com/godka/Pensieve-PPO/blob/a1b2579ca325625a23fe7d329a186ef09e32a3f1/src/ppo2.py
+"""
+
+from .agent import PPOAgent
+from .model import Actor, Critic
+
+__all__ = [
+    'PPOAgent',
+    'Actor',
+    'Critic',
+]
--- pensieve_ppo/agent/ppo/model.py
+++ pensieve_ppo/agent/ppo/model.py
@@ -30,7 +30,13 @@ class Actor(nn.Module):
         https://github.com/godka/Pensieve-PPO/blob/a1b2579ca325625a23fe7d329a186ef09e32a3f1/src/ppo2.py#L12-L41
     """
 
-    def __init__(self, state_dim: List[int], action_dim: int):
+    def __init__(
+        self,
+        state_dim: List[int],
+        action_dim: int,
+        feature_num: int = FEATURE_NUM,
+        action_eps: float = ACTION_EPS,
+    ):
         """Initialize the Actor network.
 
         Reference:
@@ -39,20 +45,25 @@ class Actor(nn.Module):
         Args:
             state_dim: State dimension as [num_features, sequence_length].
             action_dim: Number of discrete actions.
+            feature_num: Number of features in hidden layers. Defaults to 128.
+            action_eps: Small epsilon for action probability clipping. Defaults to 1e-4.
         """
         super(Actor, self).__init__()
         # Actor network
         self.s_dim = state_dim
         self.a_dim = action_dim
 
-        self.fc1_actor = nn.Linear(1, FEATURE_NUM)
-        self.fc2_actor = nn.Linear(1, FEATURE_NUM)
-        self.conv1_actor = nn.Linear(self.s_dim[1], FEATURE_NUM)
-        self.conv2_actor = nn.Linear(self.s_dim[1], FEATURE_NUM)
-        self.conv3_actor = nn.Linear(self.a_dim, FEATURE_NUM)
-        self.fc3_actor = nn.Linear(1, FEATURE_NUM)
-        self.fc4_actor = nn.Linear(FEATURE_NUM * self.s_dim[0], FEATURE_NUM)
-        self.pi_head = nn.Linear(FEATURE_NUM, action_dim)
+        self.fc1_actor = nn.Linear(1, feature_num)
+        self.fc2_actor = nn.Linear(1, feature_num)
+        self.conv1_actor = nn.Linear(self.s_dim[1], feature_num)
+        self.conv2_actor = nn.Linear(self.s_dim[1], feature_num)
+        self.conv3_actor = nn.Linear(self.a_dim, feature_num)
+        self.fc3_actor = nn.Linear(1, feature_num)
+        self.fc4_actor = nn.Linear(feature_num * self.s_dim[0], feature_num)
+        self.pi_head = nn.Linear(feature_num, action_dim)
+
+        self.feature_num = feature_num
+        self.action_eps = action_eps
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """Forward pass through the actor network.
@@ -68,16 +79,16 @@ class Actor(nn.Module):
         """
         split_0 = F.relu(self.fc1_actor(inputs[:, 0:1, -1]))
         split_1 = F.relu(self.fc2_actor(inputs[:, 1:2, -1]))
-        split_2 = F.relu(self.conv1_actor(inputs[:, 2:3, :]).view(-1, FEATURE_NUM))
-        split_3 = F.relu(self.conv2_actor(inputs[:, 3:4, :]).view(-1, FEATURE_NUM))
-        split_4 = F.relu(self.conv3_actor(inputs[:, 4:5, :self.a_dim]).view(-1, FEATURE_NUM))
+        split_2 = F.relu(self.conv1_actor(inputs[:, 2:3, :]).view(-1, self.feature_num))
+        split_3 = F.relu(self.conv2_actor(inputs[:, 3:4, :]).view(-1, self.feature_num))
+        split_4 = F.relu(self.conv3_actor(inputs[:, 4:5, :self.a_dim]).view(-1, self.feature_num))
         split_5 = F.relu(self.fc3_actor(inputs[:, 5:6, -1]))
 
         merge_net = torch.cat([split_0, split_1, split_2, split_3, split_4, split_5], 1)
 
         pi_net = F.relu(self.fc4_actor(merge_net))
         pi = F.softmax(self.pi_head(pi_net), dim=-1)
-        pi = torch.clamp(pi, ACTION_EPS, 1. - ACTION_EPS)
+        pi = torch.clamp(pi, self.action_eps, 1. - self.action_eps)
         return pi
 
 
@@ -91,7 +102,12 @@ class Critic(nn.Module):
         https://github.com/godka/Pensieve-PPO/blob/a1b2579ca325625a23fe7d329a186ef09e32a3f1/src/ppo2.py#L44-L72
     """
 
-    def __init__(self, state_dim: List[int], action_dim: int):
+    def __init__(
+        self,
+        state_dim: List[int],
+        action_dim: int,
+        feature_num: int = FEATURE_NUM,
+    ):
         """Initialize the Critic network.
 
         Reference:
@@ -100,20 +116,22 @@ class Critic(nn.Module):
         Args:
             state_dim: State dimension as [num_features, sequence_length].
             action_dim: Number of discrete actions.
+            feature_num: Number of features in hidden layers. Defaults to 128.
         """
         super(Critic, self).__init__()
         # Critic network
         self.s_dim = state_dim
         self.a_dim = action_dim
+        self.feature_num = feature_num
 
-        self.fc1_actor = nn.Linear(1, FEATURE_NUM)
-        self.fc2_actor = nn.Linear(1, FEATURE_NUM)
-        self.conv1_actor = nn.Linear(self.s_dim[1], FEATURE_NUM)
-        self.conv2_actor = nn.Linear(self.s_dim[1], FEATURE_NUM)
-        self.conv3_actor = nn.Linear(self.a_dim, FEATURE_NUM)
-        self.fc3_actor = nn.Linear(1, FEATURE_NUM)
-        self.fc4_actor = nn.Linear(FEATURE_NUM * self.s_dim[0], FEATURE_NUM)
-        self.val_head = nn.Linear(FEATURE_NUM, 1)
+        self.fc1_actor = nn.Linear(1, feature_num)
+        self.fc2_actor = nn.Linear(1, feature_num)
+        self.conv1_actor = nn.Linear(self.s_dim[1], feature_num)
+        self.conv2_actor = nn.Linear(self.s_dim[1], feature_num)
+        self.conv3_actor = nn.Linear(self.a_dim, feature_num)
+        self.fc3_actor = nn.Linear(1, feature_num)
+        self.fc4_actor = nn.Linear(feature_num * self.s_dim[0], feature_num)
+        self.val_head = nn.Linear(feature_num, 1)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """Forward pass through the critic network.
@@ -129,9 +147,9 @@ class Critic(nn.Module):
         """
         split_0 = F.relu(self.fc1_actor(inputs[:, 0:1, -1]))
         split_1 = F.relu(self.fc2_actor(inputs[:, 1:2, -1]))
-        split_2 = F.relu(self.conv1_actor(inputs[:, 2:3, :]).view(-1, FEATURE_NUM))
-        split_3 = F.relu(self.conv2_actor(inputs[:, 3:4, :]).view(-1, FEATURE_NUM))
-        split_4 = F.relu(self.conv3_actor(inputs[:, 4:5, :self.a_dim]).view(-1, FEATURE_NUM))
+        split_2 = F.relu(self.conv1_actor(inputs[:, 2:3, :]).view(-1, self.feature_num))
+        split_3 = F.relu(self.conv2_actor(inputs[:, 3:4, :]).view(-1, self.feature_num))
+        split_4 = F.relu(self.conv3_actor(inputs[:, 4:5, :self.a_dim]).view(-1, self.feature_num))
         split_5 = F.relu(self.fc3_actor(inputs[:, 5:6, -1]))
 
         merge_net = torch.cat([split_0, split_1, split_2, split_3, split_4, split_5], 1)
