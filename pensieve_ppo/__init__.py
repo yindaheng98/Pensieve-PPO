8ec1509650261629b58935854ea1b117fc11f9e8
yindaheng98 <yindaheng98@gmail.com>
2026-01-11 16:13:41 -0800

    init root module

---
+++ pensieve_ppo/__init__.py
@@ -0,0 +1,7 @@
+"""Pensieve PPO - Adaptive Bitrate Streaming with Proximal Policy Optimization.
+
+This package implements the Pensieve ABR algorithm using PPO (Proximal Policy Optimization).
+
+Reference:
+    https://github.com/godka/Pensieve-PPO
+"""
