9f406b8af28af17ace9cfc369e7b489a16c7a16e
yindaheng98 <yindaheng98@gmail.com>
2026-06-27 18:15:25 -0700

    less import

--- pensieve_ppo/core/video/__init__.py
+++ pensieve_ppo/core/video/__init__.py
@@ -1,19 +1,17 @@
 """Video data loading module."""
 
-from .envivio import (
-    EnvivioVideoChunkRequest,
-    EnvivioVideoPlayer,
-)
 from .player import VideoChunkRequest, VideoChunkRequestType, VideoPlayer
 from .registry import create_video_player, get_available_video_players, register
 
+# Import video implementations to trigger registration
+from . import envivio  # noqa: F401
+
 __all__ = [
     'create_video_player',
-    'EnvivioVideoChunkRequest',
-    'EnvivioVideoPlayer',
     'get_available_video_players',
     'register',
     'VideoChunkRequest',
     'VideoChunkRequestType',
     'VideoPlayer',
+    'envivio',
 ]
