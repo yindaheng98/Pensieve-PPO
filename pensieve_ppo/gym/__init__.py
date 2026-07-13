a512affeda771ef0d3ec795c9069c787bb06234c
yindaheng98 <yindaheng98@gmail.com>
2026-07-13 00:34:00 -0700

    export

--- pensieve_ppo/gym/__init__.py
+++ pensieve_ppo/gym/__init__.py
@@ -2,6 +2,7 @@
 
 from .env import ABREnv, AbstractABRStateObserver, State
 from .imitate import ImitationObserver, ImitationState
+from .qoe import QoEObserver, QoEState
 from .combinations import (
     create_env,
     create_env_with_class,
@@ -15,6 +16,8 @@ __all__ = [
     'State',
     'ImitationObserver',
     'ImitationState',
+    'QoEObserver',
+    'QoEState',
     'create_env',
     'create_env_with_class',
     'create_imitation_env',
